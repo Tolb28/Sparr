8fb0603a4554ac0b2f3edeaa64aca93bb6b81fb8
Tolb28 <tolb2008@gmail.com>
2025-11-11 14:05:24 +0100

    GlueStack fix

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,16 @@
+// App.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <GluestackUIProvider config={config}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </GluestackUIProvider>
   );
 }
