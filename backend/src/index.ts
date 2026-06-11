e96ab8e79753e51f1e898b1b9f0ffb3b6b1b19eb
Alex Blot <168514471+Tolb28@users.noreply.github.com>
2026-06-11 07:57:36 +0200

    Pterodactyle hosting port closed fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,6 +27,6 @@ app.use("/api/auth", authRouter);
 
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(Number(PORT), "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
 
 
